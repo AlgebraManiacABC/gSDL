3440ae770f08d7f7347c13b42d7f3882c497d5e0
AlgebraManiacABC <dunnluke01@gmail.com>
2023-05-12 04:25:41 -0500

    Test now generates a window for one second, instead of only initializing.

--- tests/init.c
+++ tests/init.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 #include <gSDL.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-	//SDL_Window * w;
-	//SDL_Renderer * r;
-	if(SDL_Init(SDL_INIT_VIDEO))
-	{
-		fprintf(stderr,"SDL_Init fail! [%s]\n",SDL_GetError());
-		return EXIT_FAILURE;
-	}
-	/*
-	if(gSDL_Init(SDL_INIT_EVERYTHING,&w,640,400,SDL_WINDOW_BORDERLESS,"Test",&r))
+	SDL_Window * w;
+	SDL_Renderer * r;
+	//if(SDL_Init(SDL_INIT_VIDEO))
+	//{
+	//	fprintf(stderr,"SDL_Init fail! [%s]\n",SDL_GetError());
+	//	return EXIT_FAILURE;
+	//}
+	if(gSDL_Init(SDL_INIT_EVERYTHING,&w,640,400,SDL_WINDOW_RESIZABLE,"Test",&r))
 	{
 		fprintf(stderr,"Initialization failure: %s\n",SDL_GetError());
 		return EXIT_FAILURE;
 	}
-	*/
 	printf("Initialization success!\n");
-	//gSDL_Close(LEVEL_REND,&r,&w);
-	//SDL_DestroyRenderer(r);
-	//SDL_DestroyWindow(w);
+	SDL_Delay(1000);
+	gSDL_Close(LEVEL_REND,&r,&w);
+	SDL_DestroyRenderer(r);
+	SDL_DestroyWindow(w);
 	SDL_Quit();
 	return EXIT_SUCCESS;
 }
